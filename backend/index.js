58b8c775cbd167245f6ff5ea7c4d686b28dfa221
farakh-shahid <farakhraja72@gmail.com>
2023-07-18 00:23:59 +0500

    implement logger middleware

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,14 @@ const { mongoose } = require('./db');
 const app = express();
 require('dotenv').config();
 app.use(bodyParser.json());
+require('colors');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+const {
+  getStatusColor,
+  getStatusMessageColor,
+  getFormattedTimestamp,
+} = require('./helpers');
 app.use(
   cors({
     origin: true,
@@ -18,6 +23,31 @@ app.use(
     credentials: true, // Enable credentials (cookies) to be sent with the request
   })
 );
+
+const createLog = (req, res, next) => {
+  const start = Date.now();
+
+  res.on('finish', function () {
+    const duration = Date.now() - start;
+    const timestamp = getFormattedTimestamp();
+    const method = req.method;
+    const url = decodeURI(req.url);
+    const statusCode = res.statusCode;
+    const statusMessage = res.statusMessage;
+    const agent = req.get('User-Agent');
+    const coloredStatus = getStatusColor(statusCode);
+    const coloredStatusMessage = getStatusMessageColor(statusMessage);
+
+    console.log(
+      `[${timestamp}] ${coloredStatus} ${method} ${url} - ${statusCode} ${coloredStatusMessage} (${duration}ms) - Agent: ${agent}`
+    );
+  });
+
+  next();
+};
+
+app.use(createLog);
+
 app.use(
   expressjwt({
     secret: process.env.SECRET_KEY,
@@ -41,13 +71,3 @@ app.listen(PORT, () => {
 
 app.use('/user', require('./routes/userRoutes'));
 app.use('/project', require('./routes/projectRoutes'));
-
-// // ----------- Sequelize Connection - Commented For Future Use (If Needed) ------------- //
-
-// const db = require('./models');
-
-// db.sequelize.sync().then(() => {
-//     app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-//     });
-// });
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,18 +1,18 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const userController = require("../controllers/userController");
-const { check, validationResult } = require("express-validator");
+const userController = require('../controllers/userController');
+const { check, validationResult } = require('express-validator');
 
 router.post(
-  "/signup",
+  '/signup',
   [
-    check("firstName", "Please Enter a Valid First Name").not().isEmpty(),
-    check("lastName", "Please Enter a Valid Last Name").not().isEmpty(),
-    check("email", "Please enter a valid email").isEmail(),
-    check("password", "Password must be atleast 6 characters long!").isLength({
+    check('firstName', 'Please Enter a Valid First Name').not().isEmpty(),
+    check('lastName', 'Please Enter a Valid Last Name').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Password must be atleast 6 characters long!').isLength({
       min: 6,
     }),
-    check("companyName", "Please Enter a Valid Company Name").not().isEmpty(),
+    check('companyName', 'Please Enter a Valid Company Name').not().isEmpty(),
   ],
   (req, res, next) => {
     const errors = validationResult(req);
@@ -25,10 +25,10 @@ router.post(
 );
 
 router.post(
-  "/login",
+  '/login',
   [
-    check("email", "Please enter a valid email").isEmail(),
-    check("password", "Please enter a valid password").isLength({
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a valid password').isLength({
       min: 6,
     }),
   ],
