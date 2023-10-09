1caf3a300e6e9778025e9f03e6430091fecf566b
farakh-shahid <farakhraja72@gmail.com>
2023-10-09 10:24:17 +0500

    checking cred

--- backend/index.js
+++ backend/index.js
@@ -66,7 +66,7 @@ app.use(function (err, req, res, next) {
 const PORT = process.env.PORT || 4001;
 
 app.listen(PORT, () => {
-  console.log(`<---Server is running on port ${PORT} --->`);
+  console.log(`<----Server is running on port ${PORT} ---->`);
 });
 
 app.use('/user', require('./routes/userRoutes'));
